a6009ebd00493160f69b1a2596a5bbb2a7cec480
MarianaPolo2019 <57376965+MarianaPolo2019@users.noreply.github.com>
2021-06-26 09:26:43 -0300

    Initial commit

---
+++ ExercLogin/Data/IESContext.cs
@@ -0,0 +1,18 @@
+﻿using ExercLogin.Models.Infra;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExercLogin.Data
+{
+    public class IESContext : IdentityDbContext<UsuarioDaAplicacao>
+    {
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+
+        }
+    }
+}
